4288a3b16840f3a71e8a2d9bde60cc69fb2ed164
Svetlaya87 <svetlaya87@gmail.com>
2021-04-26 20:22:01 +0300

    add colors to resultPlace

--- assets/js/code.js
+++ assets/js/code.js
@@ -24,18 +24,23 @@ function imt() {
 
     if ( bodyMassIndex < 16.5 ) {
         
+        resultPlace.style.color= "green";
         resultPlace.innerHTML = `Индекс массы тела ${bodyMassIndex}. Он означает: выраженный дефицит массы тела`;
         //console.log(`Индекс массы тела ${bodyMassIndex}. Это меньше 16.5. Выраженный дефицит массы тела `);
     }
 
 
     if ( bodyMassIndex >= 16.5 && bodyMassIndex <= 18.49) {
+
+        resultPlace.style.color= "green";
         resultPlace.innerHTML = `Индекс массы тела ${bodyMassIndex}. Он означает: масса тела недостаточная. `;
         //console.log(`Индекс массы тела 16.5 <= ${bodyMassIndex} <= 18.49. Масса тела недостаточная. `);
     }
 
 
     if ( bodyMassIndex >= 18.5 && bodyMassIndex <= 24.99) {
+
+        resultPlace.style.color= "blue";
 
         resultPlace.innerHTML = `Индекс массы тела ${bodyMassIndex}. Он означает: масса тела нормальная. `;
         //console.log(`Индекс массы тела ${bodyMassIndex}. Это больше 18,5 и меньше 24.99. Масса тела нормальная. `);
@@ -45,6 +50,8 @@ function imt() {
 
     if ( bodyMassIndex >= 25 && bodyMassIndex <= 29.99) {
 
+        resultPlace.style.color= "rgb(250,202,118)";
+
         resultPlace.innerHTML = `Индекс массы тела ${bodyMassIndex}. Он означает: масса тела избыточная. Стадия предожирения `;
         // console.log(`Индекс массы тела ${bodyMassIndex}. Это больше 25 и меньше 29.99. Масса тела избыточная. Стадия предожирения `);
     }
@@ -52,11 +59,16 @@ function imt() {
 
     if ( bodyMassIndex >= 30 && bodyMassIndex <= 34.99) {
 
+
+        resultPlace.style.color= "rgb(244,218,4)";
+
         resultPlace.innerHTML = `Ваш индекс массы тела ${bodyMassIndex}. Он означает: ожирение I степени. `;
         //console.log(`Ваш индекс массы тела ${bodyMassIndex}. Это больше 30  и меньше 34.99. Ожирение I степени. `);
     }
 
     if ( bodyMassIndex >= 35 && bodyMassIndex <= 39.99) {
+
+        resultPlace.style.color= "rgb(208,143,84)";
 
         resultPlace.innerHTML = `Индекс массы тела ${bodyMassIndex}. Он означает: ожирение II степени. `;
         // console.log(`Индекс массы тела ${bodyMassIndex}. Это больше 35 и меньше 39.99. Ожирение II степени. `);
@@ -64,6 +76,8 @@ function imt() {
 
 
     if ( bodyMassIndex >= 40 ) {
+
+        resultPlace.style.color= "rgb(247,35,0)";
 
         resultPlace.innerHTML = `Индекс массы тела ${bodyMassIndex}. Он означает: ожирение III степени. Морбидное`;
         // console.log(`Индекс массы тела ${bodyMassIndex}. Это больше 40. Ожирение III степени. Морбидное`);
